05e962f40abe287a9354c0308139517e3664d409
RonUme <umehara.r.ab@m.titech.ac.jp>
2023-05-12 11:54:48 +0900

    Update MoveCamera.cs

--- Assets/Script/MoveCamera.cs
+++ Assets/Script/MoveCamera.cs
@@ -6,11 +6,15 @@ public class MoveCamera : MonoBehaviour
 {
     public float moveSpeed = 2.0f;
 
+    private GameObject child;
+    private Transform childTransform;
     private Transform _cameraTransform;
 
     // Start is called before the first frame update
     void Start()
     {
+        child = transform.GetChild(0).gameObject;
+        childTransform = child.transform;
         _cameraTransform = this.gameObject.transform;
     }
 
@@ -24,12 +28,12 @@ public class MoveCamera : MonoBehaviour
     {
         Vector3 camPos = _cameraTransform.position;
 
-        if (Input.GetKey(KeyCode.D)) { camPos += _cameraTransform.right * Time.deltaTime * moveSpeed; }
-        if (Input.GetKey(KeyCode.A)) { camPos -= _cameraTransform.right * Time.deltaTime * moveSpeed; }
-        if (Input.GetKey(KeyCode.E)) { camPos += _cameraTransform.up * Time.deltaTime * moveSpeed; }
-        if (Input.GetKey(KeyCode.Q)) { camPos -= _cameraTransform.up * Time.deltaTime * moveSpeed; }
-        if (Input.GetKey(KeyCode.W)) { camPos += _cameraTransform.forward * Time.deltaTime * moveSpeed; }
-        if (Input.GetKey(KeyCode.S)) { camPos -= _cameraTransform.forward * Time.deltaTime * moveSpeed; }
+        if (Input.GetKey(KeyCode.D)) { camPos += childTransform.right * Time.deltaTime * moveSpeed; }
+        if (Input.GetKey(KeyCode.A)) { camPos -= childTransform.right * Time.deltaTime * moveSpeed; }
+        if (Input.GetKey(KeyCode.E)) { camPos += childTransform.up * Time.deltaTime * moveSpeed; }
+        if (Input.GetKey(KeyCode.Q)) { camPos -= childTransform.up * Time.deltaTime * moveSpeed; }
+        if (Input.GetKey(KeyCode.W)) { camPos += childTransform.forward * Time.deltaTime * moveSpeed; }
+        if (Input.GetKey(KeyCode.S)) { camPos -= childTransform.forward * Time.deltaTime * moveSpeed; }
 
         _cameraTransform.position = camPos;
     }
